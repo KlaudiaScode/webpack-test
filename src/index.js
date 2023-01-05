a3ac19dc148233618c17717063c2fe8ad17d661e
Klaudia Sagan <klaudiasagan.code@gmail.com>
2023-01-05 13:38:05 +0100

    added webpack,use npx webpack to run

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML =  _.join(['Hello', 'webpack', 'abc'], ' ');
+  element.innerHTML =  _.join(['Hello', 'webpack', 'abc', 'xyz'], ' ');
 
   return element;
 }
